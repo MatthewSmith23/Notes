d1423cf4f60efb63f33dc4d44067061051e7c87a
Matthew <smith0223@ymail.com>
2020-12-08 13:19:21 -0500

    delete hw & re download

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,12 +11,24 @@ router.get("/notes", (req, res) => {
 
 // POST "/api/notes" 
 // Use addNote in the store object
-// YOUR CODE HERE
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 // Use removeNote method in the store object
-// YOUR CODE HERE
+router.delete("/notes/:id", (req, res) => {
+  id = req.params.id;
+  store
+    .removeNote(id)
+    .then(() => res.json( {
+      ok: true
+    }))
+});
 
 
 
